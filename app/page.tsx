60ae8d053368f784d1151567578a54227aca6cd7
Russell Lin <superbr7789@gmail.com>
2023-11-18 21:13:20 +0800

    fulfill part of the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 import Image from "next/image";
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
-  return <h1 className="text-3xl text-teal-500">Hello World</h1>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
