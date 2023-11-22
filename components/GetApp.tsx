ebb767ad403233a533b3b3d5edf4f71c63bc4b39
Russell Lin <superbr7789@gmail.com>
2023-11-22 20:55:59 +0800

    fulfill whole CTA section

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import Image from "next/image";
 
 const GetApp = () => {
   return (
@@ -12,7 +13,7 @@ const GetApp = () => {
           <p className="regular-16 text-gray-10">
             Available on iOS and Android
           </p>
-          <div>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
             <Button
               type="button"
               title="App Store"
@@ -28,6 +29,10 @@ const GetApp = () => {
               full
             />
           </div>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" alt="phone" width={550} height={870} />
         </div>
       </div>
     </section>
